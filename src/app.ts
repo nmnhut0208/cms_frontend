c9753acd1ef7922daab2426900bdf9de639fbaa2
bigcitybear <wokhug99@gmail.com>
2024-07-03 00:18:05 +0700

    chore: update logo and remove name

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,13 @@ import { RequestConfig } from '@umijs/max';
 
 // Global initialization data configuration, used for Layout user information and permission initialization
 // For more information, see the documentation: https://umijs.org/docs/api/runtime-config#getinitialstate
-export async function getInitialState(): Promise<{ name: string }> {
-  return { name: '@umijs/max' };
+export async function getInitialState(): Promise<{ name?: string }> {
+  return {};
 }
 
 export const layout = () => {
   return {
-    logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5BRaHekzQ9yAt7kLyOctXQngSYcBVdZvqFA&s',
     menu: {
       locale: false,
     },
